3868ee6f2bfbff1a688599e255e45f98c9e9c0ce
avivdro <84494721+avivdro@users.noreply.github.com>
2021-05-31 13:41:32 +0300

    start writing events to db

--- database.py
+++ database.py
@@ -6,17 +6,65 @@ This file uses opnenpyxl to read and write to the excel
 
 import openpyxl
 from openpyxl import load_workbook
+from list_events import dtm_str_to_obj
 
 # EXCEL FILE CONSTANTS:
-DATABASE_FILE = 'database.xlsx'
-SHEET1_NAME = 'Sheet1'
+DATABASE_FILE = 'database.xlsx'  # this file needs to be in same dir
+SHEET1_NAME = 'Sheet1'           # this sheet needs to be in the file
+WEEK_23_NAME = 'week23'
+WEEK_24_NAME = 'week24'
+WEEK_25_NAME = 'week25'
+WEEK_26_NAME = 'week26'
+WEEK_1_NAME = 'week1'
+WEEK_2_NAME = 'week2'
+# add more as i go...
+
 
 # SHEET1:
 COL_A = 'A'
 
+# COL_DAYS
+COL_D1_START = 'A'  # sunday
+COL_D1_END = 'B'
+COL_D1_NAME = 'C'
+COL_D1_LEN = 'D'
+
+
+
+# ROWS
+ROW_EVENTS_START = 5
+ROW_EVENTS_END = 20
 # EXCEL SHEET STARTUP
 DB = load_workbook(DATABASE_FILE)
 SHEET1 = DB[SHEET1_NAME]
+WEEK_23_SHEET = DB[WEEK_23_NAME]
+# TODO init the other sheets
+
+
+def write_events_to_db(events):
+    """
+    writes the event to the excel database
+    :param events: list of event string as received from google calendar
+    :return:
+    """
+    i = ROW_EVENTS_START
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        end = event['end'].get('dateTime', event['end'].get('date'))
+        print("Writing event: ", event['summary'])
+        dtm_start = dtm_str_to_obj(start)
+        dtm_end = dtm_str_to_obj(end)
+        elapsed_time = dtm_end - dtm_start
+        # write from:
+        SHEET1[COL_D1_START + str(i)] = str(dtm_start)
+        # write to:
+        SHEET1[COL_D1_END + str(i)] = str(dtm_end)
+        # write name:
+        SHEET1[COL_D1_NAME + str(i)] = str(event['summary'])
+        # write length:
+        SHEET1[COL_D1_LEN + str(i)] = elapsed_time
+        i += 1
+    save()
 
 
 def save():
--- list_events.py
+++ list_events.py
@@ -1,30 +1,33 @@
 """
 aviv drori, may 2021
-base from: https://karenapp.io/articles/how-to-automate-google-calendar-with-python-using-the-calendar-api/
+base from: https://karenapp.io/articles/
+how-to-automate-google-calendar-with-python-using-the-calendar-api/
 """
 
 from datetime import datetime, time, date, timedelta
+
+import database
 from cal_setup import get_calendar_service
-import re
-# TODO  regex needed?
 
 
 CALENDAR_ID = 'c_gjgaqdjibjd14j93eannsmps3c@group.calendar.google.com'
-HOW_MANY_EVENTS = 20
+HOW_MANY_EVENTS = 60
+WEEK_DELTA = 1
 
 
-def new_get_time(dtm_string):
+def dtm_str_to_obj(dtm_string):
     """
-    @:param: dtm_string: string representing datetime, from google calendar
-    @:return: dtm: datetime object of the input
+    :param dtm_string: string representing datetime, from google calendar
+    :return: dtm: datetime object of the input
     """
     if len(dtm_string) == 10:   # like 2021-05-31
         # print("Full-day event.")
-        return datetime(2020, 1, 1, 1, 1, 1)
+        return datetime(2020, 1, 1, 1, 1, 1)  # arbitrary time
     # time_ = time_string[11:19]
     time_obj = time(int(dtm_string[11:13]), int(dtm_string[14:16], 0))
     # print(time_obj)
-    date_obj = date(int(dtm_string[0:4]), int(dtm_string[5:7]), int(dtm_string[8:10]))
+    date_obj = date(int(dtm_string[0:4]), int(dtm_string[5:7]),
+                    int(dtm_string[8:10]))
     # print(date_obj)
     dtm = datetime.combine(date_obj, time_obj)
     # print(dtm)
@@ -34,7 +37,7 @@ def new_get_time(dtm_string):
 def main():
     service = get_calendar_service()
     # Call the Calendar API
-    now = datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time  // datetime.datetime?
+    now = datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
     print('Getting List of 10 events')
     events_result = service.events().list(
         calendarId=CALENDAR_ID, timeMin=now,
@@ -44,18 +47,22 @@ def main():
 
     if not events:
         print('No upcoming events found.')
+    else:
+        database.write_events_to_db(events)
+    """
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
         end = event['end'].get('dateTime', event['end'].get('date'))
-        event_time = event['end'].get('dateTime', event['end'].get('date'))
-        # print(start, event['summary'], "TIME: ", event_time)
         print(event['summary'])
-        # print(start, "---", end)
-        dtm_start = new_get_time(start)
-        dtm_end = new_get_time(end)
-        dlt = dtm_end - dtm_start
-        print("LENGTH: ", dlt)
+        dtm_start = dtm_str_to_obj(start)
+        dtm_end = dtm_str_to_obj(end)
+        elapsed_time = dtm_end - dtm_start
+        print("LENGTH: ", elapsed_time)
+        print("WEEK NUMBER: ", dtm_start.isocalendar()[1]+1)
+        # TODO if the event is all day it translates to week number 1 (+1 =2)
+        # need to fix it.
         print("____________________________")
+    """
 
 
 if __name__ == '__main__':
